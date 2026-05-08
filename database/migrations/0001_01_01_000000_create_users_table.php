78a8df8a8370d10dd397d09d3b2c4a296058407b
Chet <zhongfyer@gmail.com>
2026-05-08 21:15:30 +0700

    make register and login

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,10 +16,8 @@ return new class extends Migration
             $table->string('first_name');
             $table->string('last_name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('role')->default('user'); // 'user' or 'admin'
-            $table->rememberToken();
             $table->timestamps();
         });
 
--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,6 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [UserController::class, 'store'])->name('api.register');
+Route::get('/users', [UserController::class, 'showalluser'])->name('api.users');
+Route::get('/users/{id}', [UserController::class, 'showuserbyid'])->name('api.user.show');
+Route::post('/login', [UserController::class, 'login'])->name('api.login');
